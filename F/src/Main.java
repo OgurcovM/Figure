b875bcd5f04e55c181d13e910d655393c54b63f7
OgurcovM <112924148+OgurcovM@users.noreply.github.com>
2022-10-03 01:44:26 +0300

    Add files via upload

---
+++ F/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Figure rectangle = new Rectangle(5.0f, 5.0f);
+        System.out.printf("%.12f", rectangle.getPerimeter());
+    }
+}
